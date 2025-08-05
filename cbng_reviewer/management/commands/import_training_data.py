153bd1f127790df69775275cfadfa3adebe4c944
Damian Zaremba <damian@damianzaremba.co.uk>
2025-08-05 23:19:04 +0000

    import_training_data - check for deletion

--- cbng_reviewer/management/commands/import_training_data.py
+++ cbng_reviewer/management/commands/import_training_data.py
@@ -5,6 +5,7 @@ from typing import Any
 from django.core.management import BaseCommand, CommandParser
 
 from cbng_reviewer.libs.edit_set.utils import import_training_data
+from cbng_reviewer.libs.wikipedia.reader import WikipediaReader
 from cbng_reviewer.libs.wikipedia.training import WikipediaTraining
 from cbng_reviewer.models import Edit
 
@@ -12,6 +13,11 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    def __init__(self, *args, **kwargs):
+        self._wikipedia_reader = WikipediaReader()
+        self._wikipedia_training = WikipediaTraining()
+        super(Command, self).__init__(*args, **kwargs)
+
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument("--workers", type=int, default=5)
         parser.add_argument("--edit-id")
@@ -22,7 +28,13 @@ class Command(BaseCommand):
             logger.debug(f"Already have training data for {edit.id}")
             return
 
-        wp_edit = WikipediaTraining().build_wp_edit(edit)
+        if self._wikipedia_reader.has_revision_been_deleted(edit.id):
+            logger.info(f"Found deleted revision, skipping training data and marking as deleted")
+            edit.deleted = True
+            edit.save()
+            return
+
+        wp_edit = self._wikipedia_training.build_wp_edit(edit)
         if wp_edit.has_complete_training_data:
             logger.info(f"Importing training data from {wp_edit}")
             import_training_data(edit, wp_edit)
