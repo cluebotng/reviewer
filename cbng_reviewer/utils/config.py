2da96a27db0fd8c0a65bb0e7dbf1bc1e502b603f
Damian Zaremba <damian@damianzaremba.co.uk>
2025-09-05 18:39:32 +0000

    config - add test defaults

--- cbng_reviewer/utils/config.py
+++ cbng_reviewer/utils/config.py
@@ -13,7 +13,7 @@ def _load_yaml(path: Path) -> Dict[str, Any]:
     return {}
 
 
-def load_config(base_dir: Path) -> Dict[str, Any]:
+def load_config(base_dir: Path, in_test: bool) -> Dict[str, Any]:
     # Defaults
     cfg = {
         "django": {
@@ -25,29 +25,29 @@ def load_config(base_dir: Path) -> Dict[str, Any]:
                 "schema": os.environ.get("TOOL_DB_SCHEMA", "cbng_reviewer"),
                 "host": os.environ.get("TOOL_DB_HOST", "localhost"),
                 "port": 3306,
-                "user": os.environ.get("TOOL_DB_USER", "root"),
-                "password": os.environ.get("TOOL_DB_PASSWORD", ""),
+                "user": os.environ.get("TOOL_DB_USER", "cbng_reviewer"),
+                "password": os.environ.get("TOOL_DB_PASSWORD"),
             },
             "replica": {
                 "schema": os.environ.get("TOOL_REPLICA_SCHEMA", "enwiki_p"),
                 "host": os.environ.get("TOOL_REPLICA_HOST", "enwiki.labsdb"),
                 "port": 3306,
-                "user": os.environ.get("TOOL_REPLICA_USER", ""),
-                "password": os.environ.get("TOOL_REPLICA_PASSWORD", ""),
+                "user": os.environ.get("TOOL_REPLICA_USER", "cbng_reviewer"),
+                "password": os.environ.get("TOOL_REPLICA_PASSWORD"),
             },
         },
         "oauth": {
-            "key": os.environ.get("OAUTH_KEY", None),
-            "secret": os.environ.get("OAUTH_SECRET", None),
+            "key": os.environ.get("OAUTH_KEY"),
+            "secret": os.environ.get("OAUTH_SECRET"),
         },
         "wikipedia": {
-            "username": os.environ.get("WIKIPEDIA_USERNAME", None),
-            "password": os.environ.get("WIKIPEDIA_PASSWORD", None),
+            "username": os.environ.get("WIKIPEDIA_USERNAME"),
+            "password": os.environ.get("WIKIPEDIA_PASSWORD"),
         },
         "cbng": {
-            "admin_only": os.environ.get("CBNG_ADMIN_ONLY", "") == "true",
-            "enable_irc_messaging": os.environ.get("CBNG_ENABLE_IRC_MESSAGING", "true") == "true",
-            "enable_user_messaging": os.environ.get("CBNG_ENABLE_USER_MESSAGING", "") == "true",
+            "admin_only": os.environ.get("CBNG_ADMIN_ONLY") == "true",
+            "enable_irc_messaging": os.environ.get("CBNG_ENABLE_IRC_MESSAGING") == "true",
+            "enable_user_messaging": os.environ.get("CBNG_ENABLE_USER_MESSAGING") == "true",
         },
         "irc_relay": {
             "host": os.environ.get("IRC_RELAY_HOST", "irc-relay"),
@@ -61,7 +61,20 @@ def load_config(base_dir: Path) -> Dict[str, Any]:
             "password": os.environ.get("REDIS_PASSWORD"),
         },
     }
-    cfg = always_merger.merge(cfg, _load_yaml(base_dir / "config.yaml"))
+
+    if in_test:
+        # Testing defaults
+        cfg["mysql"]["default"] |= {"host": "127.0.0.1", "port": 3306}
+        cfg["mysql"]["replica"] |= {"host": "127.0.0.1", "port": 3306}
+        cfg["redis"] |= {"host": "localhost", "port": 6379}
+        cfg["cbng"] |= {"enable_irc_messaging": False, "enable_user_messaging": False}
+
+    else:
+        # Load local settings if the file exists
+        cfg = always_merger.merge(cfg, _load_yaml(base_dir / "config.yaml"))
+
+    # Load override settings
     if runtime_cfg := os.environ.get("CBNG_REVIEWER_CONFIG"):
         cfg = always_merger.merge(cfg, _load_yaml(Path(runtime_cfg)))
+
     return cfg
