01325df77814897f5b1526f388fbcca233d179af
Damian Zaremba <damian@damianzaremba.co.uk>
2025-09-05 17:56:54 +0000

    add_edits_to_queue - re-work logic
    - Treat ad-hoc edits in the same way as sampled edits
    - Emit more contextual logging

--- cbng_reviewer/management/commands/add_edits_to_queue.py
+++ cbng_reviewer/management/commands/add_edits_to_queue.py
@@ -16,32 +16,33 @@ class Command(BaseCommand):
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument("--edit-id")
 
+    def _ensure_edit_exists(self, edit_group: EditGroup, edit_id: int) -> None:
+        edit, created = Edit.objects.get_or_create(id=edit_id)
+        if created:
+            logger.info(f"Created entry for {edit.id}")
+
+        if not edit.groups.filter(pk=edit_group.pk).exists():
+            logger.info(f"Adding edit {edit.id} to {edit_group.name}")
+            edit.groups.add(edit_group)
+            edit.save()
+
     def handle(self, *args: Any, **options: Any) -> None:
         """Add sampled edits for review."""
-        if options["edit_id"]:
-            edit, created = Edit.objects.get_or_create(id=options["edit_id"])
-            logger.info(f"Created entry for {edit.id}")
-            return
-
-        wikipedia_reader = WikipediaReader()
-
         edit_group, created = EditGroup.objects.get_or_create(name=settings.CBNG_SAMPLED_EDITS_EDIT_SET)
         if created:
             logger.info(f"Created target edit group: {edit_group.name}")
             edit_group.weight = 40
             edit_group.save()
 
-        current_time = datetime.now()
-        for edit_id in wikipedia_reader.get_sampled_edits(
-            settings.WIKIPEDIA_NAMESPACE_NAME_TO_ID["main"],
-            current_time - timedelta(days=settings.CBNG_SAMPLED_EDITS_LOOKBACK_DAYS),
-            current_time,
-            settings.CBNG_SAMPLED_EDITS_QUANTITY,
-        ):
-            edit, created = Edit.objects.get_or_create(id=edit_id)
-            if created:
-                logger.info(f"Created entry for {edit.id}")
-                edit.groups.add(edit_group)
+        if options["edit_id"]:
+            self._ensure_edit_exists(edit_group, options["edit_id"])
+            return
 
-                logger.info(f"Triggering fetch of training data for {edit.id}")
-                tasks.import_training_data.apply_async([edit.id])
+        end_time = datetime.now()
+        start_time = end_time - timedelta(days=settings.CBNG_SAMPLED_EDITS_LOOKBACK_DAYS)
+        namespace_id = settings.WIKIPEDIA_NAMESPACE_NAME_TO_ID["main"]
+        quantity = settings.CBNG_SAMPLED_EDITS_QUANTITY
+
+        logger.info(f"Sampling {quantity} edits from ns {namespace_id} between {start_time} and {end_time}")
+        for edit_id in WikipediaReader().get_sampled_edits(namespace_id, start_time, end_time, quantity):
+            self._ensure_edit_exists(edit_group, edit_id)
