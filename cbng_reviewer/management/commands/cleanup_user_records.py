99e6fb7a765f93cb3e7b66ba624483e9dbd9ba2e
Damian Zaremba <damian@damianzaremba.co.uk>
2026-02-14 15:29:49 +0000

    cleanup_user_records - offload classification check to db

--- cbng_reviewer/management/commands/cleanup_user_records.py
+++ cbng_reviewer/management/commands/cleanup_user_records.py
@@ -3,9 +3,9 @@ from datetime import datetime, timedelta, UTC
 from typing import Any
 
 from django.conf import settings
-from django.db.models import Q
+from django.db.models import Count, Q
 
-from cbng_reviewer.models import User, Classification
+from cbng_reviewer.models import User
 from cbng_reviewer.utils.command import CommandWithMetrics
 
 logger = logging.getLogger(__name__)
@@ -15,15 +15,12 @@ class Command(CommandWithMetrics):
     def handle(self, *args: Any, **options: Any) -> None:
         """Cleanup user records for those with no rights."""
         cutoff_limit = datetime.now(tz=UTC) - timedelta(days=settings.CBNG_CLEANUP_USER_DAYS)
-        for user in User.objects.filter(
+        for user in User.objects.annotate(classification_count=Count("classification_set")).filter(
             Q(date_joined__lte=cutoff_limit)
+            & Q(classification_count=0)
             & ~Q(is_admin=True)
             & ~Q(is_reviewer=True)
             & ~Q(historical_edit_count__gt=0)
         ):
-            if Classification.objects.filter(user=user).count() > 0:
-                logger.debug(f"Skipping removal of {user.username} due to contributions")
-                continue
-
             logger.info(f"Removing (pending) user record for {user.username} ({user.date_joined})")
             user.delete()
