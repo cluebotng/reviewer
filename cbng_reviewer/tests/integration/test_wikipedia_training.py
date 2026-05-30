9e85ec7a306c07bc392ac337560bf93fdc3612f0
Damian Zaremba <damian@damianzaremba.co.uk>
2026-05-30 19:58:16 +0200

    tests - mock page revision request

--- cbng_reviewer/tests/integration/test_wikipedia_training.py
+++ cbng_reviewer/tests/integration/test_wikipedia_training.py
@@ -1,3 +1,5 @@
+from unittest.mock import MagicMock, patch
+
 from django.test import TestCase
 
 from cbng_reviewer.libs.wikipedia.training import WikipediaTraining
@@ -16,7 +18,53 @@ class WikipediaTrainingTestCase(TestCase):
         self.assertIsNone(title)
         self.assertIsNone(namespace)
 
-    def testGetPageRevisions(self):
+    @patch("requests.Session.get")
+    def testGetPageRevisions(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.raise_for_status.return_value = None
+        mock_response.json.return_value = {
+            "query": {
+                "pages": {
+                    "29881390": {
+                        "pageid": 29881390,
+                        "ns": 2,
+                        "title": "User:ClueBot NG/ReviewInterface/Stats",
+                        "revisions": [
+                            {
+                                "revid": 1070107186,
+                                "parentid": 1068228521,
+                                "user": "ClueBot NG",
+                                "timestamp": "2022-02-05T18:35:18Z",
+                                "comment": "Uploading Stats",
+                                "slots": {
+                                    "main": {
+                                        "contentmodel": "wikitext",
+                                        "contentformat": "text/x-wiki",
+                                        "*": "{{/EditGroupHeader}}\n{{/EditGroupFooter}}",
+                                    }
+                                },
+                            },
+                            {
+                                "revid": 1068228521,
+                                "parentid": 1067818664,
+                                "user": "ClueBot NG",
+                                "timestamp": "2022-01-27T09:15:29Z",
+                                "comment": "Uploading Stats",
+                                "slots": {
+                                    "main": {
+                                        "contentmodel": "wikitext",
+                                        "contentformat": "text/x-wiki",
+                                        "*": "{{/EditGroupHeader}}\n{{/EditGroupFooter}}",
+                                    }
+                                },
+                            },
+                        ],
+                    }
+                }
+            }
+        }
+        mock_get.return_value = mock_response
+
         wikipedia_training = WikipediaTraining()
         current, previous = wikipedia_training.get_page_revisions("User:ClueBot NG/ReviewInterface/Stats", 1070107186)
         self.assertTrue(current.has_complete_training_data)
